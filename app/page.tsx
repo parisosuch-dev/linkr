aa65653ccd970a0fa40a5346ad41f1b5bc72196a
parisosuch-dev <parisosuch.dev@gmail.com>
2023-11-10 11:47:19 -0800

    Add retroscreen component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
+import RetroScreen from "@/components/retroscreen"
+
 export default function Home() {
   return (
     <div className="text-black bg-gray-100 min-h-screen flex flex-col items-center justify-center">
       <div>
-        <div className="flex flex-col font-medium text-5xl border-4 rounded-sm border-black">
-          <p>./create tiny urls.</p>
-          <p>./keep them forever.</p>
-          <p>./thats it.</p>
-        </div>
+        <RetroScreen />
       </div>
     </div>
   )
---
+++ components/retroscreen.tsx
@@ -0,0 +1,11 @@
+const RetroScreen = () => {
+    return (
+        <div className="flex flex-col font-medium text-5xl border-4 rounded-sm border-black">
+            <p>./create tiny urls.</p>
+            <p>./keep them forever.</p>
+            <p>./thats it.</p>
+        </div>
+    );
+};
+
+export default RetroScreen;
