b669d0c62466643af877299295ec40f527642a7a
parisosuch-dev <parisosuch.dev@gmail.com>
2023-11-10 14:56:37 -0800

    Add typewriting effect

--- components/retroscreen.tsx
+++ components/retroscreen.tsx
@@ -1,9 +1,11 @@
+import TypeWriter from "./typewriter";
+
 const RetroScreen = () => {
     return (
         <div className="flex flex-col font-medium h-96 shadow-[7px_7px_0_0] shadow-primary justify-center space-y-8 text-5xl w-full border-4 rounded-sm border-primary">
-            <p>./create tiny urls.</p>
-            <p>./keep them forever.</p>
-            <p>./thats it.</p>
+            <TypeWriter text="./create tiny urls." delay={100} />
+            <TypeWriter text="./keep them forever." delay={100} />
+            <TypeWriter text="./thats it." delay={100} />
         </div>
     );
 };
