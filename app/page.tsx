36014d9d83c591512303fccaee40e668f9d833c3
parisosuch-dev <parisosuch.dev@gmail.com>
2023-11-10 14:39:22 -0800

    some page progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
+import Header from "@/components/ui/header"
 import RetroScreen from "@/components/retroscreen"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div className="text-black bg-gray-100 min-h-screen flex flex-col items-center justify-center">
-      <div>
+    <div className="text-primary bg-gray-100 min-h-screen flex flex-col items-center justify-center">
+      <div className="w-1/2 flex flex-col items-start">
+        <Header text="linkr" />
         <RetroScreen />
+        <div className="flex flex-row space-x-2 w-full items-center pt-6">
+          <Input placeholder="enter url here:" />
+          <Button>shorten</Button>
+        </div>
       </div>
     </div>
   )
