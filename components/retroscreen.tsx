8e20343eeddf431da975d5a6620ab2f04d80c0db
parisosuch-dev <parisosuch.dev@gmail.com>
2023-11-10 14:39:01 -0800

    oops

--- components/retroscreen.tsx
+++ components/retroscreen.tsx
@@ -1,6 +1,6 @@
 const RetroScreen = () => {
     return (
-        <div className="flex flex-col font-medium h-96 shadow-[5px_5px_0_0] shadow-primary justify-center space-y-8 text-5xl w-full border-4 rounded-sm border-primary">
+        <div className="flex flex-col font-medium h-96 shadow-[7px_7px_0_0] shadow-primary justify-center space-y-8 text-5xl w-full border-4 rounded-sm border-primary">
             <p>./create tiny urls.</p>
             <p>./keep them forever.</p>
             <p>./thats it.</p>
