1482ef3356062421b187709f7c9e0075bc71f91c
parisosuch-dev <parisosuch.dev@gmail.com>
2023-11-10 11:33:39 -0800

    Add shadcn ui library

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import Input from "@/components/Input"
-
 export default function Home() {
   return (
-    <div className="text-black min-h-screen flex flex-col items-center justify-center">
+    <div className="text-black bg-gray-100 min-h-screen flex flex-col items-center justify-center">
       <div>
-        <h1 className="font-black text-9xl">linkr</h1>
         <div className="flex flex-col font-medium text-5xl border-4 rounded-sm border-black">
           <p>./create tiny urls.</p>
           <p>./keep them forever.</p>
