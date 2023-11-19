05a4042162464cf96bed37e6acb7b226baaed501
parisosuch-dev <parisosuch.dev@gmail.com>
2023-11-19 14:49:21 -0800

    Add console out

---
+++ components/consoleout.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { useState } from "react";
+import { RiClipboardLine, RiClipboardFill } from "react-icons/ri";
+
+const ConsoleOut = ({ text, isError }: { text: string, isError?: boolean }) => {
+    const [copy, setCopy] = useState(false);
+
+    if (isError) {
+        return (
+            <p className="pt-4 font-bold text-md sm:text-lg">{"> " + text}</p>
+        )
+    }
+    return (
+        <div className="flex flex-row items-end justify-center space-x-2">
+            <p className="pt-4 font-bold text-md sm:text-lg">&gt; <a className="underline" href={text}>{text}</a></p>
+            {copy ?
+                <RiClipboardFill className="h-6 w-6 sm:h-8 sm:w-8" onClick={() => {
+                    navigator.clipboard.writeText(text);
+                }} />
+                :
+                <RiClipboardLine className="h-6 w-6 sm:h-8 sm:w-8" onClick={() => {
+                    navigator.clipboard.writeText(text);
+                    setCopy(true);
+                }} />
+            }
+
+        </div>
+
+    )
+
+}
+
+export default ConsoleOut;
