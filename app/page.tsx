99aec77759f8c0e1f58e9dd7aa33c49366e097a1
parisosuch-dev <parisosuch.dev@gmail.com>
2023-11-18 20:33:55 -0800

    check for user session

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,31 @@
-"use client"
+'use client'
 
+import { useEffect, useState } from "react"
+import { useRouter } from "next/router"
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
+import type { User } from "@supabase/auth-helpers-nextjs"
+
+// components
 import Header from "@/components/ui/header"
 import RetroScreen from "@/components/retroscreen"
 import NotAuthPopup from "@/components/notauthpopup"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import { useState } from "react"
+
+
 
 export default function Home() {
-  const [isAuth, setAuth] = useState(false);
+  const supabase = createClientComponentClient();
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const getUser = async () => {
+      supabase.auth.getUser().then(({ data: { user } }) => {
+        setUser(user);
+      });
+    }
+    getUser();
+  }, [])
 
   return (
     <div className="text-primary bg-gray-100 min-h-screen flex flex-col items-center justify-center">
@@ -17,7 +34,7 @@ export default function Home() {
         <RetroScreen />
         <div className="flex flex-row space-x-2 w-full items-center pt-4 sm:pt-6">
           <Input placeholder="$ enter url here:" />
-          {isAuth ? <Button>shorten</Button> : <NotAuthPopup/>}
+          {user ? <Button>shorten</Button> : <NotAuthPopup />}
         </div>
       </div>
     </div >
