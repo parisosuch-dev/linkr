5e465f51dcfc0a8733046aa48b000fd5fc028449
parisosuch-dev <parisosuch.dev@gmail.com>
2023-11-19 12:08:14 -0800

    add functionality to post url on page.

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,15 @@ import RetroScreen from "@/components/retroscreen"
 import NotAuthPopup from "@/components/notauthpopup"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import postLink from "@/lib/supabase/postLink"
 
 
 
 export default function Home() {
   const supabase = createClientComponentClient();
   const [user, setUser] = useState<User | null>(null);
+  const [url, setUrl] = useState('');
+  const [errorMessage, setErrorMessage] = useState<string | undefined>('');
 
   useEffect(() => {
     const getUser = async () => {
@@ -33,9 +36,21 @@ export default function Home() {
         <Header text="~linkr" />
         <RetroScreen />
         <div className="flex flex-col space-y-2 sm:space-y-0 sm:flex-row space-x-0 sm:space-x-2 w-full items-center pt-4 sm:pt-6">
-          <Input placeholder="$ enter url here:" />
-          {user ? <Button className="w-full sm:w-1/6">shorten</Button> : <NotAuthPopup />}
+          <Input placeholder="$ enter url here:" onChange={(e) => setUrl(e.target.value)} />
+          {user ?
+            <Button
+              onClick={() => {
+                // post link to db
+                postLink({ url, user }).then((res) => {
+                  setErrorMessage(res);
+                });
+              }}
+              className="w-full sm:w-1/6">shorten</Button>
+            :
+            <NotAuthPopup />
+          }
         </div>
+        <p className="pt-4 font-bold text-sm sm:text-lg">{errorMessage}</p>
       </div>
     </div >
   )
