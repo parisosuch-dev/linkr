7a6d92991a8b07a8be17c4b9b9f1da7bc15be1f8
parisosuch-dev <parisosuch.dev@gmail.com>
2023-11-23 09:21:46 -0800

    remove typewriter functionality

--- components/retroscreen.tsx
+++ components/retroscreen.tsx
@@ -1,11 +1,9 @@
-import TypeWriter from "./typewriter";
-
 const RetroScreen = () => {
     return (
         <div className="flex flex-col font-medium h-48 sm:h-96 shadow-[5px_5px_0_0] sm:shadow-[7px_7px_0_0] shadow-primary justify-center space-y-1 sm:space-y-8 text-xl sm:text-5xl w-full border-2 sm:border-4 rounded-sm border-primary">
-            <TypeWriter text="./create tiny urls." delay={50} />
-            <TypeWriter text="./keep them forever." delay={50} />
-            <TypeWriter text="./thats it." delay={50} />
+            <p>./create tiny urls.</p>
+            <p>./keep them forever.</p>
+            <p>./thats it.</p>
         </div>
     );
 };
