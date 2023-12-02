bd517bf6dc89aa864492b77a82416d3dc81cf9b4
parisosuch-dev <parisosuch.dev@gmail.com>
2023-12-02 13:33:31 -0800

    Add delete confirmation popup

--- app/(front)/links/page.tsx
+++ app/(front)/links/page.tsx
@@ -4,19 +4,20 @@ import { useState, useEffect } from "react"
 import Header from "@/components/ui/header"
 import getUserLinks from "@/lib/supabase/getUserLinks"
 import { User } from "@supabase/supabase-js"
-import type Link from "@/lib/supabase/Link"
+import type Linkr from "@/lib/supabase/Linkr"
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
 import { redirect } from "next/navigation"
 import LinkControl from "@/components/linkcontrol"
+import { Button } from "@/components/ui/button"
+import { useRouter } from "next/navigation"
 
 export default function Links() {
-    const [user, setUser] = useState<User | null>(null);
-    const [links, setLinks] = useState<Link[] | null>(null);
+    const [links, setLinks] = useState<Linkr[] | null>(null);
     const supabase = createClientComponentClient();
+    const router = useRouter();
 
     useEffect(() => {
         supabase.auth.getUser().then(({ data: { user } }) => {
-            setUser(user);
             if (user) {
                 getUserLinks(user).then((res) => {
                     setLinks(res);
@@ -29,16 +30,30 @@ export default function Links() {
                 redirect("/");
             }
         });
-    }, [])
+    });
+
+    const noLinks = (
+        <div className="flex flex-row space-x-4 items-center justify-center">
+            <p className="w-1/2 text-lg sm:text-3xl ">you have no links!</p>
+            <Button
+                className="w-1/2"
+                onClick={() => {
+                    console.log("here")
+                    router.push("/");
+                }}>add one</Button>
+        </div>
+    )
 
     return (
         <div className="text-primary bg-gray-100 flex flex-col items-center justify-center pt-0 pb-16 sm:pb-32">
             <div className="w-5/6 sm:w-1/2 flex flex-col items-start">
                 <Header text="~links" />
                 <div className="shadow-[5px_5px_0_0] sm:shadow-[7px_7px_0_0] shadow-primary space-y-6 sm:space-y-8 w-full border-2 sm:border-4 rounded-sm border-primary p-2 sm:p-8">
-                    {links ? links.map((value, index) => (
-                        <LinkControl key={index} link={value} />
-                    )) : <p className="text-lg sm:text-2xl">loading links...</p>}
+                    {links && links.length === 0 ? noLinks
+                        : links ? links.map((value, index) => (
+                            <LinkControl key={index} link={value} />
+                        ))
+                            : <p className="text-lg sm:text-2xl">loading links...</p>}
                 </div>
             </div>
         </div>
--- components/linkcontrol.tsx
+++ components/linkcontrol.tsx
@@ -1,16 +1,57 @@
-import Link from "@/lib/supabase/Link";
-import { Button } from "./ui/button";
+'use client'
 
-export default function LinkControl({ link }: { link: Link }) {
+import Linkr from "@/lib/supabase/Linkr";
+import { Button } from "./ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogClose,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+
+export default function LinkControl({ link }: { link: Linkr }) {
     const date = new Date(link.created_at);
     const timestamp = date.toLocaleDateString() + " " + date.toLocaleTimeString();
+    const supabase = createClientComponentClient();
+
+    const deleteLink = async () => {
+        // delete link for user
+        const { error } = await supabase.from('link').delete().eq('id', link.id);
+
+        if (error) {
+            return false;
+        }
+        return true
+    }
     return (
         <div className="grid grid-cols-2 justify-items-center items-center">
             <div className="w-1/2">
                 <a className="underline font-bold text-lg sm:text-2xl p-2" href={link.url}>{link.url}</a>
-                <p className="border-4 bg-gray-200 rounded-full w-max sm:px-2 text-xs sm:text-lg">{timestamp}</p>
+                <p className="border-4 bg-gray-200 rounded-full w-max px-0.5 sm:px-2 text-xs sm:text-lg">{timestamp}</p>
             </div>
-            <Button className="w-1/2">delete</Button>
+            <Dialog>
+                <DialogTrigger asChild>
+                    <Button className="w-1/2">delete</Button>
+                </DialogTrigger>
+                <DialogContent className="sm:w-full shadow-[5px_5px_0_0] border-2 sm:border-4 border-primary">
+                    <DialogHeader>
+                        <DialogTitle className="text-xl sm:text-5xl font-medium text-center pt-2">Are you sure you want to delete your link?</DialogTitle>
+                        <DialogDescription className="text-sm sm:text-lg text-center">
+                            This will permanently delete your short link: {`${link.url}`}
+                        </DialogDescription>
+                    </DialogHeader>
+                    <DialogClose asChild>
+                        <Button onClick={() => {
+                            deleteLink();
+                        }}>Delete the link</Button>
+                    </DialogClose>
+                </DialogContent>
+            </Dialog>
         </div>
     );
 }
