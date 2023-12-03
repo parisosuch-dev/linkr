fc16edfd38e0a7a35f4bde84fd09630d892c451a
parisosuch-dev <parisosuch.dev@gmail.com>
2023-12-02 16:02:48 -0800

    Add button hover transition

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-sm shadow-[5px_5px_0_0] shadow-[7px_7px_0_0] shadow-primary text-sm sm:text-lg font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center rounded-sm shadow-[5px_5px_0_0] shadow-[7px_7px_0_0] hover:translate-y-2 hover:translate-x-2 transition ease-in-out  hover:shadow-none shadow-primary text-sm sm:text-lg font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
